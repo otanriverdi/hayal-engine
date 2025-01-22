5ceb7ce0ef8b5fde16b0a79186ef581fa9d63c91
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2025-01-23 00:46:23 +0300

    small refactor

--- src/game.zig
+++ src/game.zig
@@ -4,16 +4,16 @@ pub const OffscreenBuffer = struct {
     const Self = @This();
 
     pixels: []u8,
-    width: usize,
-    height: usize,
-    pitch: usize,
+    width: u64,
+    height: u64,
+    pitch: u64,
     allocator: std.mem.Allocator,
 
-    pub fn init(width: usize, height: usize, pitch: usize, allocator: std.mem.Allocator) !Self {
+    pub fn init(width: u64, height: u64, allocator: std.mem.Allocator) !Self {
         return Self{
             .allocator = allocator,
             .pixels = try allocator.alloc(u8, width * height * 4),
-            .pitch = pitch,
+            .pitch = width * 4,
             .width = width,
             .height = height,
         };
@@ -24,4 +24,29 @@ pub const OffscreenBuffer = struct {
     }
 };
 
-pub fn UpdateAndRender(_: *OffscreenBuffer) !void {}
+var x_offset: u64 = 0;
+var y_offset: u64 = 0;
+
+pub fn UpdateAndRender(buffer: *OffscreenBuffer) !void {
+    var row_index: usize = 0;
+    for (0..buffer.height) |y| {
+        for (0..buffer.width) |x| {
+            const red = @as(u8, @intCast((x + x_offset) & 0xFF));
+            const green = @as(u8, @intCast((y + y_offset) & 0xFF));
+            const blue = @as(u8, @intCast((x ^ y) & 0xFF));
+            const alpha = 0xFF;
+
+            const pixel_index = row_index + (x * 4);
+
+            buffer.pixels[pixel_index + 0] = red;
+            buffer.pixels[pixel_index + 1] = green;
+            buffer.pixels[pixel_index + 2] = blue;
+            buffer.pixels[pixel_index + 3] = alpha;
+        }
+        row_index += buffer.pitch;
+    }
+
+    // Simple animation by moving the offsets
+    x_offset = (x_offset + 1) % buffer.width;
+    y_offset = (y_offset + 1) % buffer.height;
+}
--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,7 @@ const c = @cImport({
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
+    if (c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO) != 0) {
         c.SDL_LogError(c.SDL_LOG_CATEGORY_ERROR, "Unable to initialize SDL: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
     }
@@ -25,6 +25,15 @@ pub fn main() !void {
     };
     defer c.SDL_DestroyRenderer(renderer);
 
+    var window_width: c_int = undefined;
+    var window_height: c_int = undefined;
+    c.SDL_GetWindowSize(window, &window_width, &window_height);
+
+    var offscreen_buffer = try game.OffscreenBuffer.init(@as(usize, @intCast(window_width)), @as(usize, @intCast(window_height)), allocator);
+    var texture = c.SDL_CreateTexture(renderer, c.SDL_PIXELFORMAT_RGBA8888, c.SDL_TEXTUREACCESS_STREAMING, window_width, window_height) orelse {
+        return error.SDLTextureFailed;
+    };
+
     var quit = false;
     while (!quit) {
         var event: c.SDL_Event = undefined;
@@ -34,33 +43,29 @@ pub fn main() !void {
                     quit = true;
                 },
                 c.SDL_KEYUP, c.SDL_KEYDOWN => {
-                    var was_down = false;
-                    if (event.key.state == c.SDL_RELEASED) {
-                        was_down = true;
-                    } else if (event.key.repeat != 0) {
-                        was_down = true;
-                    }
-                    c.SDL_Log("Key press = %i was_down = %d", event.key.keysym.sym, was_down);
+                    c.SDL_Log("Key press = %i was_down = %d", event.key.keysym.sym);
+                },
+                c.SDL_WINDOWEVENT_RESIZED => {
+                    c.SDL_GetWindowSize(window, &window_width, &window_height);
+                    offscreen_buffer.deinit();
+                    offscreen_buffer = try game.OffscreenBuffer.init(@as(usize, @intCast(window_width)), @as(usize, @intCast(window_height)), allocator);
+
+                    c.SDL_DestroyTexture(texture);
+                    texture = c.SDL_CreateTexture(renderer, c.SDL_PIXELFORMAT_RGBA8888, c.SDL_TEXTUREACCESS_STREAMING, window_width, window_height) orelse {
+                        return error.SDLTextureFailed;
+                    };
                 },
                 else => {},
             }
         }
-        var window_width: c_int = undefined;
-        var window_height: c_int = undefined;
-        c.SDL_GetWindowSize(window, &window_width, &window_height);
 
-        var offscreen_buffer = try game.OffscreenBuffer.init(@as(usize, @intCast(window_width)), @as(usize, @intCast(window_height)), 0, allocator);
-        defer offscreen_buffer.deinit();
         try game.UpdateAndRender(&offscreen_buffer);
 
-        const texture = c.SDL_CreateTexture(renderer, c.SDL_PIXELFORMAT_ARGB8888, c.SDL_TEXTUREACCESS_STREAMING, window_width, window_height) orelse {
-            return error.SDLTextureFailed;
-        };
-        defer c.SDL_DestroyTexture(texture);
-        _ = c.SDL_UpdateTexture(texture, 0, @ptrCast(&offscreen_buffer.pixels), 0);
-
+        _ = c.SDL_UpdateTexture(texture, 0, @ptrCast(offscreen_buffer.pixels), @intCast(offscreen_buffer.pitch));
         _ = c.SDL_RenderClear(renderer);
         _ = c.SDL_RenderCopy(renderer, texture, 0, 0);
         c.SDL_RenderPresent(renderer);
     }
+
+    c.SDL_Quit();
 }

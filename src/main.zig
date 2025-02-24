e9f72a6fee3b53ecb09e006317930f1dbb3ee4a7
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2025-02-24 16:16:12 +0300

    refactor the sdl platform layer

--- src/main.zig
+++ src/main.zig
@@ -5,14 +5,6 @@ const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
 
-const TARGET_FRAME_RATE = 60;
-const AUDIO_SAMPLES_PER_SECOND = 48000;
-const AUDIO_CHANNELS = 2;
-
-fn createTexture(renderer: ?*c.SDL_Renderer, width: c_int, height: c_int) ?*c.SDL_Texture {
-    return c.SDL_CreateTexture(renderer, c.SDL_PIXELFORMAT_RGBA8888, c.SDL_TEXTUREACCESS_TARGET, width, height);
-}
-
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -22,42 +14,39 @@ pub fn main() !void {
     }
 
     if (c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO) != 0) {
-        return error.SDLInitializationFailed;
+        return error.SDLInitFailed;
     }
     defer c.SDL_Quit();
 
-    const window = c.SDL_CreateWindow("game", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, 1920, 1080, c.SDL_WINDOW_OPENGL) orelse {
-        return error.SDLInitializationFailed;
+    const window = createSdlWindow() orelse {
+        return error.SDLWindowInitFailed;
     };
     defer c.SDL_DestroyWindow(window);
 
-    const renderer = c.SDL_CreateRenderer(window, -1, c.SDL_RENDERER_ACCELERATED | c.SDL_RENDERER_PRESENTVSYNC) orelse {
-        return error.SDLInitializationFailed;
+    const renderer = createSdlRenderer(window) orelse {
+        return error.SDLRendererInitFailed;
     };
     defer c.SDL_DestroyRenderer(renderer);
 
-    const audio_samples_per_frame = AUDIO_SAMPLES_PER_SECOND / TARGET_FRAME_RATE;
-    var audio_spec: c.SDL_AudioSpec = .{
-        .channels = AUDIO_CHANNELS,
-        .format = c.AUDIO_S16LSB,
-        .freq = AUDIO_SAMPLES_PER_SECOND,
-        .samples = audio_samples_per_frame * @sizeOf(i16) * AUDIO_CHANNELS,
+    const AUDIO_SAMPLES_PER_SECOND = 48000;
+    const TARGET_FRAME_RATE = 60;
+
+    const audio_device = openSdlAudioDevice(AUDIO_SAMPLES_PER_SECOND, TARGET_FRAME_RATE) orelse {
+        return error.SDLAudioDeviceInitFailed;
     };
-    const audio_device = c.SDL_OpenAudioDevice(null, 0, &audio_spec, null, 0);
-    if (audio_device == 0) {
-        return error.SDLAudioDeviceFailed;
-    }
     c.SDL_PauseAudioDevice(audio_device, 0);
-    var sound_buffer = try platform.SoundBuffer.init(audio_samples_per_frame, allocator);
+    var sound_buffer = try platform.SoundBuffer.init(AUDIO_SAMPLES_PER_SECOND / TARGET_FRAME_RATE, allocator);
     defer sound_buffer.deinit();
 
     var window_width: c_int = undefined;
     var window_height: c_int = undefined;
+
     c.SDL_GetWindowSize(window, &window_width, &window_height);
     var offscreen_buffer = try platform.OffscreenBuffer.init(@as(usize, @intCast(window_width)), @as(usize, @intCast(window_height)), allocator);
     defer offscreen_buffer.deinit();
-    var texture = createTexture(renderer, window_width, window_height) orelse {
-        return error.SDLTextureFailed;
+
+    var texture = createSdlTexture(renderer, window_width, window_height) orelse {
+        return error.SDLTextureInitFailed;
     };
 
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -81,8 +70,7 @@ pub fn main() !void {
             }
         }
 
-        var input = platform.Input.init();
-
+        var input: platform.Input = undefined;
         var event: c.SDL_Event = undefined;
         while (c.SDL_PollEvent(&event) != 0) {
             switch (event.type) {
@@ -90,45 +78,14 @@ pub fn main() !void {
                     quit = true;
                 },
                 c.SDL_KEYUP, c.SDL_KEYDOWN => {
-                    var is_down = false;
-                    if (event.key.state == c.SDL_RELEASED) {
-                        is_down = true;
-                    }
-                    var current_input: ?*platform.KeyState = null;
-                    switch (event.key.keysym.scancode) {
-                        c.SDL_SCANCODE_W, c.SDL_SCANCODE_UP => {
-                            current_input = &input.up;
-                        },
-                        c.SDL_SCANCODE_A, c.SDL_SCANCODE_LEFT => {
-                            current_input = &input.left;
-                        },
-                        c.SDL_SCANCODE_D, c.SDL_SCANCODE_RIGHT => {
-                            current_input = &input.right;
-                        },
-                        c.SDL_SCANCODE_S, c.SDL_SCANCODE_DOWN => {
-                            current_input = &input.down;
-                        },
-                        c.SDL_SCANCODE_SPACE => {
-                            current_input = &input.main_button;
-                        },
-                        c.SDL_SCANCODE_INSERT => {
-                            current_input = &input.secondary_button;
-                        },
-                        else => {},
-                    }
-                    if (current_input) |i| {
-                        if (i.has_ended_down != is_down) {
-                            i.half_transition_count += 1;
-                        }
-                        i.has_ended_down = is_down;
-                    }
+                    input = parseSdlEventToInput(event);
                 },
                 c.SDL_WINDOWEVENT_RESIZED => {
                     c.SDL_GetWindowSize(window, &window_width, &window_height);
                     offscreen_buffer.deinit();
                     offscreen_buffer = try platform.OffscreenBuffer.init(@as(usize, @intCast(window_width)), @as(usize, @intCast(window_height)), allocator);
                     c.SDL_DestroyTexture(texture);
-                    texture = createTexture(renderer, window_width, window_height) orelse {
+                    texture = createSdlTexture(renderer, window_width, window_height) orelse {
                         return error.SDLTextureFailed;
                     };
                 },
@@ -146,6 +103,70 @@ pub fn main() !void {
         _ = c.SDL_RenderCopy(renderer, texture, 0, 0);
         c.SDL_RenderPresent(renderer);
     }
+}
 
-    c.SDL_Quit();
+fn createSdlTexture(renderer: ?*c.SDL_Renderer, width: c_int, height: c_int) ?*c.SDL_Texture {
+    return c.SDL_CreateTexture(renderer, c.SDL_PIXELFORMAT_RGBA8888, c.SDL_TEXTUREACCESS_TARGET, width, height);
+}
+
+fn createSdlWindow() ?*c.SDL_Window {
+    return c.SDL_CreateWindow("game", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, 1920, 1080, c.SDL_WINDOW_OPENGL);
+}
+
+fn createSdlRenderer(window: ?*c.SDL_Window) ?*c.SDL_Renderer {
+    return c.SDL_CreateRenderer(window, -1, c.SDL_RENDERER_ACCELERATED | c.SDL_RENDERER_PRESENTVSYNC);
+}
+
+fn openSdlAudioDevice(samples_per_second: u16, target_frame_rate: u16) ?c.SDL_AudioDeviceID {
+    const audio_channels = 2;
+    const audio_samples_per_frame = samples_per_second / target_frame_rate;
+    var audio_spec: c.SDL_AudioSpec = .{
+        .channels = 2,
+        .format = c.AUDIO_S16LSB,
+        .freq = samples_per_second,
+        .samples = audio_samples_per_frame * @sizeOf(u16) * audio_channels,
+    };
+
+    const device_id = c.SDL_OpenAudioDevice(null, 0, &audio_spec, null, 0);
+    return if (device_id != 0) device_id else null;
+}
+
+fn parseSdlEventToInput(event: c.SDL_Event) platform.Input {
+    std.debug.assert(event.type == c.SDL_KEYDOWN or event.type == c.SDL_KEYUP);
+
+    var input = platform.Input.init();
+    var is_down = false;
+    if (event.key.state == c.SDL_RELEASED) {
+        is_down = true;
+    }
+    var current_input: ?*platform.KeyState = null;
+    switch (event.key.keysym.scancode) {
+        c.SDL_SCANCODE_W, c.SDL_SCANCODE_UP => {
+            current_input = &input.up;
+        },
+        c.SDL_SCANCODE_A, c.SDL_SCANCODE_LEFT => {
+            current_input = &input.left;
+        },
+        c.SDL_SCANCODE_D, c.SDL_SCANCODE_RIGHT => {
+            current_input = &input.right;
+        },
+        c.SDL_SCANCODE_S, c.SDL_SCANCODE_DOWN => {
+            current_input = &input.down;
+        },
+        c.SDL_SCANCODE_SPACE => {
+            current_input = &input.main_button;
+        },
+        c.SDL_SCANCODE_INSERT => {
+            current_input = &input.secondary_button;
+        },
+        else => {},
+    }
+    if (current_input) |i| {
+        if (i.has_ended_down != is_down) {
+            i.half_transition_count += 1;
+        }
+        i.has_ended_down = is_down;
+    }
+
+    return input;
 }

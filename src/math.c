0d1377d684154e5742719bd6b75ed22c3724c590
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2025-06-23 21:50:55 +0300

    add vec4 to the math library

--- src/math.c
+++ src/math.c
@@ -31,6 +31,36 @@ vec2 vec2_normalize(vec2 v) {
   return (vec2){v.x / len, v.y / len};
 }
 
+vec4 vec4_add(vec4 a, vec4 b) { return (vec4){a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w}; }
+
+vec4 vec4_sub(vec4 a, vec4 b) { return (vec4){a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w}; }
+
+vec4 vec4_mul(vec4 a, vec4 b) { return (vec4){a.x * b.x, a.y * b.y, a.z * b.z, a.w * b.w}; }
+
+vec4 vec4_div(vec4 a, vec4 b) { return (vec4){a.x / b.x, a.y / b.y, a.z / b.z, a.w / b.w}; }
+
+vec4 vec4_add_scalar(vec4 v, float s) { return (vec4){v.x + s, v.y + s, v.z + s, v.w + s}; }
+
+vec4 vec4_sub_scalar(vec4 v, float s) { return (vec4){v.x - s, v.y - s, v.z - s, v.w - s}; }
+
+vec4 vec4_mul_scalar(vec4 v, float s) { return (vec4){v.x * s, v.y * s, v.z * s, v.w * s}; }
+
+vec4 vec4_div_scalar(vec4 v, float s) { return (vec4){v.x / s, v.y / s, v.z / s, v.w / s}; }
+
+float vec4_dot(vec4 a, vec4 b) { return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w; }
+
+float vec4_length(vec4 v) { return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w); }
+
+float vec4_length_sq(vec4 v) { return v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w; }
+
+vec4 vec4_normalize(vec4 v) {
+  float len = vec4_length(v);
+  if (len == 0.0f) {
+    return (vec4){0.0f, 0.0f, 0.0f, 0.0f};
+  }
+  return (vec4){v.x / len, v.y / len, v.z / len, v.w / len};
+}
+
 rgba_float rgba_div_scalar(rgba rgba, float s) {
   return (rgba_float){rgba.r / s, rgba.g / s, rgba.b / s, rgba.a / s};
 }
--- src/math.h
+++ src/math.h
@@ -7,6 +7,10 @@ typedef struct vec2 {
   float x, y;
 } vec2;
 
+typedef struct vec4 {
+  float x, y, z, w;
+} vec4;
+
 typedef struct rgba {
   uint8_t r, g, b, a;
 } rgba;
@@ -29,6 +33,21 @@ float vec2_dot(vec2 a, vec2 b);
 float vec2_length(vec2 v);
 float vec2_length_sq(vec2 v);
 vec2 vec2_normalize(vec2 v);
+
+vec4 vec4_add(vec4 a, vec4 b);
+vec4 vec4_sub(vec4 a, vec4 b);
+vec4 vec4_mul(vec4 a, vec4 b);
+vec4 vec4_div(vec4 a, vec4 b);
+
+vec4 vec4_add_scalar(vec4 v, float s);
+vec4 vec4_sub_scalar(vec4 v, float s);
+vec4 vec4_mul_scalar(vec4 v, float s);
+vec4 vec4_div_scalar(vec4 v, float s);
+
+float vec4_dot(vec4 a, vec4 b);
+float vec4_length(vec4 v);
+float vec4_length_sq(vec4 v);
+vec4 vec4_normalize(vec4 v);
 
 rgba_float rgba_div_scalar(rgba rgba, float s);
 

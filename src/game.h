c112aeff056847b3635c20ff387d986ed451936b
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2025-07-05 14:21:18 +0300

    improve input handling

--- src/game.h
+++ src/game.h
@@ -7,15 +7,108 @@
 #include <stdint.h>
 
 enum GameKeys {
+  KEY_UNKNOWN = 0,
+
+  // Movement keys
   KEY_W = 1,
   KEY_A,
   KEY_S,
   KEY_D,
+
+  // Arrow keys
+  KEY_UP,
+  KEY_DOWN,
+  KEY_LEFT,
+  KEY_RIGHT,
+
+  // Action keys
   KEY_SPACE,
   KEY_ENTER,
   KEY_ESC,
+  KEY_LSHIFT,
+  KEY_RSHIFT,
+  KEY_LCTRL,
+  KEY_RCTRL,
+  KEY_LALT,
+  KEY_RALT,
+  KEY_TAB,
+  KEY_BACKSPACE,
+  KEY_DELETE,
+
+  // Number keys
+  KEY_1,
+  KEY_2,
+  KEY_3,
+  KEY_4,
+  KEY_5,
+  KEY_6,
+  KEY_7,
+  KEY_8,
+  KEY_9,
+  KEY_0,
+
+  // Letter keys
+  KEY_Q,
+  KEY_E,
+  KEY_R,
+  KEY_T,
+  KEY_Y,
+  KEY_U,
+  KEY_I,
+  KEY_O,
+  KEY_P,
+  KEY_F,
+  KEY_G,
+  KEY_H,
+  KEY_J,
+  KEY_K,
+  KEY_L,
+  KEY_Z,
+  KEY_X,
+  KEY_C,
+  KEY_V,
+  KEY_B,
+  KEY_N,
+  KEY_M,
+
+  // Function keys
+  KEY_F1,
+  KEY_F2,
+  KEY_F3,
+  KEY_F4,
+  KEY_F5,
+  KEY_F6,
+  KEY_F7,
+  KEY_F8,
+  KEY_F9,
+  KEY_F10,
+  KEY_F11,
+  KEY_F12,
+
+  // Keypad keys
+  KEY_KP_0,
+  KEY_KP_1,
+  KEY_KP_2,
+  KEY_KP_3,
+  KEY_KP_4,
+  KEY_KP_5,
+  KEY_KP_6,
+  KEY_KP_7,
+  KEY_KP_8,
+  KEY_KP_9,
+  KEY_KP_ENTER,
+  KEY_KP_PLUS,
+  KEY_KP_MINUS,
+  KEY_KP_MULTIPLY,
+  KEY_KP_DIVIDE,
+  KEY_KP_DECIMAL,
+
+  // Mouse buttons
   KEY_MOUSE1,
   KEY_MOUSE2,
+  KEY_MOUSE3,
+  KEY_MOUSE4,
+  KEY_MOUSE5,
 
   KEY_COUNT,
 };
@@ -30,6 +123,8 @@ typedef struct game_input {
   int mouse_y;
   int mouse_dx;
   int mouse_dy;
+  int mouse_wheel_x;
+  int mouse_wheel_y;
   game_key_state keys[KEY_COUNT];
 } game_input;
 
--- src/main_linux.c
+++ src/main_linux.c
@@ -83,6 +83,8 @@ int main() {
 
     input.mouse_dx = 0;
     input.mouse_dy = 0;
+    input.mouse_wheel_x = 0;
+    input.mouse_wheel_y = 0;
     SDL_Event event;
     while (SDL_PollEvent(&event) != 0) {
       parse_sdl_event(window, &event, &input, &renderer, &should_quit);
@@ -122,7 +124,6 @@ void parse_sdl_event(SDL_Window *window, SDL_Event *event, game_input *input, re
       SDL_GL_GetDrawableSize(window, (int *)&renderer->framebuffer_size.x,
                              (int *)&renderer->framebuffer_size.y);
       glViewport(0, 0, (int)renderer->framebuffer_size.x, (int)renderer->framebuffer_size.y);
-      // }
       break;
 
     case SDL_KEYDOWN:
@@ -130,82 +131,307 @@ void parse_sdl_event(SDL_Window *window, SDL_Event *event, game_input *input, re
       if (event->key.repeat)
         break;
       bool isDown = (event->type == SDL_KEYDOWN);
+      int key_index = KEY_UNKNOWN;
+
       switch (event->key.keysym.sym) {
-      case SDLK_w: {
-        game_key_state *k = &input->keys[KEY_W];
-        if (k->is_down != isDown) {
-          k->is_down = isDown;
-          k->half_transition_count++;
-        }
-      } break;
-      case SDLK_a: {
-        game_key_state *k = &input->keys[KEY_A];
-        if (k->is_down != isDown) {
-          k->is_down = isDown;
-          k->half_transition_count++;
-        }
-      } break;
-      case SDLK_s: {
-        game_key_state *k = &input->keys[KEY_S];
-        if (k->is_down != isDown) {
-          k->is_down = isDown;
-          k->half_transition_count++;
-        }
-      } break;
-      case SDLK_d: {
-        game_key_state *k = &input->keys[KEY_D];
-        if (k->is_down != isDown) {
-          k->is_down = isDown;
-          k->half_transition_count++;
-        }
-      } break;
-      case SDLK_SPACE: {
-        game_key_state *k = &input->keys[KEY_SPACE];
-        if (k->is_down != isDown) {
-          k->is_down = isDown;
-          k->half_transition_count++;
-        }
-      } break;
-      case SDLK_RETURN: {
-        game_key_state *k = &input->keys[KEY_ENTER];
-        if (k->is_down != isDown) {
-          k->is_down = isDown;
-          k->half_transition_count++;
-        }
-      } break;
-      case SDLK_ESCAPE: {
-        game_key_state *k = &input->keys[KEY_ESC];
-        if (k->is_down != isDown) {
-          k->is_down = isDown;
-          k->half_transition_count++;
-        }
-      } break;
+      // Movement keys
+      case SDLK_w:
+        key_index = KEY_W;
+        break;
+      case SDLK_a:
+        key_index = KEY_A;
+        break;
+      case SDLK_s:
+        key_index = KEY_S;
+        break;
+      case SDLK_d:
+        key_index = KEY_D;
+        break;
+
+      // Arrow keys
+      case SDLK_UP:
+        key_index = KEY_UP;
+        break;
+      case SDLK_DOWN:
+        key_index = KEY_DOWN;
+        break;
+      case SDLK_LEFT:
+        key_index = KEY_LEFT;
+        break;
+      case SDLK_RIGHT:
+        key_index = KEY_RIGHT;
+        break;
+
+      // Action keys
+      case SDLK_SPACE:
+        key_index = KEY_SPACE;
+        break;
+      case SDLK_RETURN:
+        key_index = KEY_ENTER;
+        break;
+      case SDLK_ESCAPE:
+        key_index = KEY_ESC;
+        break;
+      case SDLK_LSHIFT:
+        key_index = KEY_LSHIFT;
+        break;
+      case SDLK_RSHIFT:
+        key_index = KEY_RSHIFT;
+        break;
+      case SDLK_LCTRL:
+        key_index = KEY_LCTRL;
+        break;
+      case SDLK_RCTRL:
+        key_index = KEY_RCTRL;
+        break;
+      case SDLK_LALT:
+        key_index = KEY_LALT;
+        break;
+      case SDLK_RALT:
+        key_index = KEY_RALT;
+        break;
+      case SDLK_TAB:
+        key_index = KEY_TAB;
+        break;
+      case SDLK_BACKSPACE:
+        key_index = KEY_BACKSPACE;
+        break;
+      case SDLK_DELETE:
+        key_index = KEY_DELETE;
+        break;
+
+      // Number keys
+      case SDLK_1:
+        key_index = KEY_1;
+        break;
+      case SDLK_2:
+        key_index = KEY_2;
+        break;
+      case SDLK_3:
+        key_index = KEY_3;
+        break;
+      case SDLK_4:
+        key_index = KEY_4;
+        break;
+      case SDLK_5:
+        key_index = KEY_5;
+        break;
+      case SDLK_6:
+        key_index = KEY_6;
+        break;
+      case SDLK_7:
+        key_index = KEY_7;
+        break;
+      case SDLK_8:
+        key_index = KEY_8;
+        break;
+      case SDLK_9:
+        key_index = KEY_9;
+        break;
+      case SDLK_0:
+        key_index = KEY_0;
+        break;
+
+      // Letter keys
+      case SDLK_q:
+        key_index = KEY_Q;
+        break;
+      case SDLK_e:
+        key_index = KEY_E;
+        break;
+      case SDLK_r:
+        key_index = KEY_R;
+        break;
+      case SDLK_t:
+        key_index = KEY_T;
+        break;
+      case SDLK_y:
+        key_index = KEY_Y;
+        break;
+      case SDLK_u:
+        key_index = KEY_U;
+        break;
+      case SDLK_i:
+        key_index = KEY_I;
+        break;
+      case SDLK_o:
+        key_index = KEY_O;
+        break;
+      case SDLK_p:
+        key_index = KEY_P;
+        break;
+      case SDLK_f:
+        key_index = KEY_F;
+        break;
+      case SDLK_g:
+        key_index = KEY_G;
+        break;
+      case SDLK_h:
+        key_index = KEY_H;
+        break;
+      case SDLK_j:
+        key_index = KEY_J;
+        break;
+      case SDLK_k:
+        key_index = KEY_K;
+        break;
+      case SDLK_l:
+        key_index = KEY_L;
+        break;
+      case SDLK_z:
+        key_index = KEY_Z;
+        break;
+      case SDLK_x:
+        key_index = KEY_X;
+        break;
+      case SDLK_c:
+        key_index = KEY_C;
+        break;
+      case SDLK_v:
+        key_index = KEY_V;
+        break;
+      case SDLK_b:
+        key_index = KEY_B;
+        break;
+      case SDLK_n:
+        key_index = KEY_N;
+        break;
+      case SDLK_m:
+        key_index = KEY_M;
+        break;
+
+      // Function keys
+      case SDLK_F1:
+        key_index = KEY_F1;
+        break;
+      case SDLK_F2:
+        key_index = KEY_F2;
+        break;
+      case SDLK_F3:
+        key_index = KEY_F3;
+        break;
+      case SDLK_F4:
+        key_index = KEY_F4;
+        break;
+      case SDLK_F5:
+        key_index = KEY_F5;
+        break;
+      case SDLK_F6:
+        key_index = KEY_F6;
+        break;
+      case SDLK_F7:
+        key_index = KEY_F7;
+        break;
+      case SDLK_F8:
+        key_index = KEY_F8;
+        break;
+      case SDLK_F9:
+        key_index = KEY_F9;
+        break;
+      case SDLK_F10:
+        key_index = KEY_F10;
+        break;
+      case SDLK_F11:
+        key_index = KEY_F11;
+        break;
+      case SDLK_F12:
+        key_index = KEY_F12;
+        break;
+
+      // Keypad keys
+      case SDLK_KP_0:
+        key_index = KEY_KP_0;
+        break;
+      case SDLK_KP_1:
+        key_index = KEY_KP_1;
+        break;
+      case SDLK_KP_2:
+        key_index = KEY_KP_2;
+        break;
+      case SDLK_KP_3:
+        key_index = KEY_KP_3;
+        break;
+      case SDLK_KP_4:
+        key_index = KEY_KP_4;
+        break;
+      case SDLK_KP_5:
+        key_index = KEY_KP_5;
+        break;
+      case SDLK_KP_6:
+        key_index = KEY_KP_6;
+        break;
+      case SDLK_KP_7:
+        key_index = KEY_KP_7;
+        break;
+      case SDLK_KP_8:
+        key_index = KEY_KP_8;
+        break;
+      case SDLK_KP_9:
+        key_index = KEY_KP_9;
+        break;
+      case SDLK_KP_ENTER:
+        key_index = KEY_KP_ENTER;
+        break;
+      case SDLK_KP_PLUS:
+        key_index = KEY_KP_PLUS;
+        break;
+      case SDLK_KP_MINUS:
+        key_index = KEY_KP_MINUS;
+        break;
+      case SDLK_KP_MULTIPLY:
+        key_index = KEY_KP_MULTIPLY;
+        break;
+      case SDLK_KP_DIVIDE:
+        key_index = KEY_KP_DIVIDE;
+        break;
+      case SDLK_KP_PERIOD:
+        key_index = KEY_KP_DECIMAL;
+        break;
+
       default:
         break;
+      }
+
+      if (key_index != KEY_UNKNOWN) {
+        game_key_state *k = &input->keys[key_index];
+        if (k->is_down != isDown) {
+          k->is_down = isDown;
+          k->half_transition_count++;
+        }
       }
     } break;
 
     case SDL_MOUSEBUTTONDOWN:
     case SDL_MOUSEBUTTONUP: {
       bool isDown = (event->type == SDL_MOUSEBUTTONDOWN);
-      switch (event->button.button) {
-      case SDL_BUTTON_LEFT: {
-        game_key_state *k = &input->keys[KEY_MOUSE1];
-        if (k->is_down != isDown) {
-          k->is_down = isDown;
-          k->half_transition_count++;
-        }
-      } break;
-      case SDL_BUTTON_RIGHT: {
-        game_key_state *k = &input->keys[KEY_MOUSE2];
-        if (k->is_down != isDown) {
-          k->is_down = isDown;
-          k->half_transition_count++;
-        }
-      } break;
+      int key_index = KEY_UNKNOWN;
 
+      switch (event->button.button) {
+      case SDL_BUTTON_LEFT:
+        key_index = KEY_MOUSE1;
+        break;
+      case SDL_BUTTON_RIGHT:
+        key_index = KEY_MOUSE2;
+        break;
+      case SDL_BUTTON_MIDDLE:
+        key_index = KEY_MOUSE3;
+        break;
+      case SDL_BUTTON_X1:
+        key_index = KEY_MOUSE4;
+        break;
+      case SDL_BUTTON_X2:
+        key_index = KEY_MOUSE5;
+        break;
       default:
         break;
+      }
+
+      if (key_index != KEY_UNKNOWN) {
+        game_key_state *k = &input->keys[key_index];
+        if (k->is_down != isDown) {
+          k->is_down = isDown;
+          k->half_transition_count++;
+        }
       }
     } break;
 
@@ -214,6 +440,11 @@ void parse_sdl_event(SDL_Window *window, SDL_Event *event, game_input *input, re
       input->mouse_y = event->motion.y;
       input->mouse_dx += event->motion.xrel;
       input->mouse_dy += event->motion.yrel;
+    } break;
+
+    case SDL_MOUSEWHEEL: {
+      input->mouse_wheel_x = event->wheel.x;
+      input->mouse_wheel_y = event->wheel.y;
     } break;
     }
   }
